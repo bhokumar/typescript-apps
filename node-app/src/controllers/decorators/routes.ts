d37cf4c20196a8d0cf3524288aa6fe4e7aaa55b4
bhokumar <bhoopendra.akgec@gmail.com>
2020-02-20 19:50:29 +0530

    Adding files

--- node-app/src/controllers/decorators/routes.ts
+++ node-app/src/controllers/decorators/routes.ts
@@ -1,10 +1,15 @@
 import 'reflect-metadata';
 import {Methods} from './methods';
 import {MetaDataKeys} from './MetaDataKeys';
+import { RequestHandler } from 'express';
+
+interface RouteHandlerDescriptor extends PropertyDescriptor {
+    value?: RequestHandler
+}
 
 function routeBinder(method: string) {
     return function (path: String) {
-        return function(target: any, key: string, desc: PropertyDescriptor) {
+        return function(target: any, key: string, desc: RouteHandlerDescriptor) {
             Reflect.defineMetadata(MetaDataKeys.path, path, target, key);
             Reflect.defineMetadata(MetaDataKeys.method, method, target, key);
         }
